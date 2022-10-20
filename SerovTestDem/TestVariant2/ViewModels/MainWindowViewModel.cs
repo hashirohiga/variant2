9d16de7885df7066d50cdd803ba1f82dc28e3842
unknown <mi.serov2016@yandex.ru>
2022-10-20 17:03:51 +0300

    хочу 4

---
+++ SerovTestDem/TestVariant2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestVariant2.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private List<ProductMaterial> _productMaterialList;
+
+        public List<ProductMaterial> ProductMaterialList
+        {
+            get { return _productMaterialList; }
+            set { _productMaterialList = value; }
+        }
+        public MainWindowViewModel()
+        {
+            using (SEROV2Context db = new SEROV2Context ())
+            {
+                ProductMaterialList = db.ProductMaterials
+                    .Include(m => m.Material)
+                    .Include(p => p.Product)
+                    .ThenInclude(pt => pt.ProductType).ToList();
+            }
+        }
+
+    }
+}
